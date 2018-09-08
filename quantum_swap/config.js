0b005e1fa74288f88e19c61b89553e65ef5ba6e9
Dex Hunter <dixingxu@gmail.com>
2018-09-08 22:54:05 +0800

    config.js

---
+++ quantum_swap/config.js
@@ -0,0 +1,6 @@
+export default {
+    'a_btc_addr': '2N7zWpgzJXb5M7KjeybmhyUJWFk8HqEF1en',
+    'b_btc_addr': '2N7xE5HuP1vdJShhrpJFnqu6od3PvUUVvYY',
+    'a_qtum_addr': 'qHrZn3GHp9spToTFkjZ4cfbzdDThw3pLce',
+    'b_qtum_addr': 'qS2iorv44FbopcxVB36gmyiEBeU4SwwyBn'
+}
