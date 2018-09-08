64c74eca50f73e1f62cae9964398629df25403a0
5sWind <fiveswind@gmail.com>
2018-09-08 18:55:05 +0800

    make

---
+++ frontend/main.js
@@ -0,0 +1,16 @@
+const bitcoin = require("bitcoinjs-lib");
+
+function setAddress (address) {
+  this.address = address;
+}
+
+function getAddress () {
+  const keyPair = bitcoin.ECPair.fromWIF(cTB14gEhBznkica1ucJQtLjrPNcKi7AbzugFhY4zvEF26SvXRKMc);
+  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+  return { address: address };
+}
+
+module.exports = {
+  getAddress: getAddress
+}
+
